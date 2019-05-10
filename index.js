ae4bcb2c2e5dd18255f2382ca0afed68c3a4bca3
Nolan <nolan.hellyer@gmail.com>
2019-05-10 16:34:15 -0700

    runs but does not query as expected

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const expressGraphql = require("express-graphql");
+const { buildSchema } = require("graphql");
 const app = require("express")();
 const fs = require("fs");
 
 const data = require("./data.json");
-const schema = fs.readFileSync("./schema.gql");
+const schema = buildSchema(fs.readFileSync("./schema.gql", "utf-8"));
 
 const PORT = 3000;
 
@@ -15,15 +16,20 @@ const getItems = type => {
 	return data[type];
 };
 
-// const root = {
-// 	Captain: getItem()
-// }
+const rootValue = {
+	captain: id => getItem(id, "Captain"),
+	captains: () => getItems("Captain"),
+	ship: id => getItem(id, "Ship"),
+	ships: () => getItems("Ship")
+};
 
-// app.use("/graphql", 
-	// expressGraphql(
-
-	// )
-// );
+app.use("/graphql", 
+	expressGraphql({
+		schema,
+		rootValue,
+		graphiql: true
+	})
+);
 
 app.listen(PORT, _ => {
 	console.log("listening on " + PORT);
